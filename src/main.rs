bc09725bffbd337972d7cdff9255245cdfb1befa
Akinator31 <paveldewavrechin@gmail.com>
2026-05-31 18:50:17 +0200

    feat(args): add argument's parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser , Debug)]
+#[command(name = "zappy_server")]
+#[command(about = "Zappy server", long_about = None)]
+pub struct ServerConfig {
+    #[arg(short = 'p')]
+    pub port: u16,
+
+    #[arg(short = 'x')]
+    pub width: u32,
+
+    #[arg(short = 'y')]
+    pub height: u32,
+
+    #[arg(short = 'n', num_args = 1..)]
+    pub teams: Vec<String>,
+
+    #[arg(short = 'c')]
+    pub clients_nb: usize,
+
+    #[arg(short = 'f', default_value_t = 100)]
+    pub freq: u32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = ServerConfig::parse();
+
+    Ok(())
 }
