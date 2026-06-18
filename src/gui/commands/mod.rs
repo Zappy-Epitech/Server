841fc6b87615d152617bf43373af1db976fd4f1f
Akinator31 <99099121+Akinator31@users.noreply.github.com>
2026-06-18 15:39:56 +0200

    feat(gui): add sgt and sst command

--- src/gui/commands/mod.rs
+++ src/gui/commands/mod.rs
@@ -1,17 +1,21 @@
 pub mod map;
 pub mod player;
+pub mod time;
 
 use crate::game::world::World;
 use crate::config::ServerConfig;
 use crate::protocol::gui::GuiCommand;
 
-pub fn execute(command: GuiCommand, world: &World, config: &ServerConfig) -> Vec<String> {
+pub fn execute(command: GuiCommand, world: &mut World, config: &mut ServerConfig) -> Vec<String> {
     match command {
         GuiCommand::MapSize | GuiCommand::TileContent(_, _) | GuiCommand::MapContent | GuiCommand::TeamNames => {
             map::execute(command, world, config)
         }
         GuiCommand::PlayerPosition(_) | GuiCommand::PlayerLevel(_) | GuiCommand::PlayerInventory(_) => {
             player::execute(command, world)
+        }
+        GuiCommand::TimeRequest | GuiCommand::TimeUpdate(_) => {
+            time::execute(command, world, config)
         }
         _ => vec!["suc\n".to_string()],
     }
